e004f42118d5b2fe69618b640b4a1deb313738d0
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-02-26 21:02:56 -0600

    updated death :-()

--- src/javascripts/components/death.js
+++ src/javascripts/components/death.js
@@ -1,5 +1,8 @@
 const death = () => {
-  document.querySelector('#app').innerHTML = 'YOU ARE DEAD!';
+  document.querySelector('#app').innerHTML = `<div class="d-flex death"><h1 class="text-white">Your Pet DIED!</h1>
+  <div><img src="https://media1.tenor.com/images/cfcf8abd561bfdbf6c03396f6c14e3aa/tenor.gif?itemid=11565777"></div>
+  <div class="mt-3"><a href="/" class="btn btn-info">Try Again</a></div>
+  </div>`;
 };
 
 export default death;
