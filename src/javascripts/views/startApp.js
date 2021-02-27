6d031ab1c315fcad9fb9423326d0e04e8ee76ba7
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-02-26 21:25:56 -0600

    removed reference to domevents

--- src/javascripts/views/startApp.js
+++ src/javascripts/views/startApp.js
@@ -2,13 +2,11 @@ import domBuilder from '../components/domBuilder';
 import modal from '../components/modal';
 import { addPet } from '../components/pet';
 import buildQuadrant from '../components/quadrantBuilder';
-import domEvents from '../events/domEvents';
 import getAverage from '../helpers/data/average';
 import quadrantData from '../helpers/data/quadrantData';
 
 const startApp = () => {
   domBuilder();
-  domEvents();
   getAverage();
   modal();
   addPet();
